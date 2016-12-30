6dee2b12b3c34ed07ec216d0f2bc766d75ccb895
MostefaBen <bennaceurmostefa@gmail.com>
2016-12-30 16:14:23 +0100

    uploading project

---
+++ Control the states of the computer.cpp
@@ -0,0 +1,48 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+void main() {
+    
+    system("COLOR 9");
+    cout << endl << "1 - Shutdown the computer." << endl
+           << endl << "2 - Shutdown the computer without warning the user." << endl
+           << endl << "3 - Cancel the shutdown of the computer." << endl
+            << endl << "4 - Restart the computer." << endl
+            << endl << "5 - Hibernate." << endl
+            << endl << "6 - Log out." << endl
+            << endl << "7 - Exit." << endl;
+
+    cout << endl << "Put your choice : " << endl;
+    char c;
+    cin >> c;
+    system("cls");
+
+    switch (c) {
+
+        case '1':system("SHUTDOWN /S /T 00");
+            break;
+
+        case '2':system("SHUTDOWN /f /S /T 00");
+            break;
+
+        case '3':system("SHUTDOWN /a");
+            break;
+
+        case '4':system("SHUTDOWN /R /T 00");
+            break;
+
+        case '5':system("SHUTDOWN /h");
+            break;
+
+        case '6':system("SHUTDOWN /L");
+            break;
+
+        case '7':system("EXIT");
+            break;
+
+        default:cout << "Error, Put again" << endl;
+    }
+
+}
